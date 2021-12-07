20afa54002e50d2e63f170a02299b33badbd17a3
Ricardo Camacho <rscamachodev@gmail.com>
2021-12-07 16:02:32 -0300

    melhorando o ambiente de desenvolvimento.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Olá!")
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request: Request, response: Response, next: NextFunction) => {
+  return response.send('Olá, mundos!');
+});
+
+app.listen(3333, () => console.log('Server is running.'));
